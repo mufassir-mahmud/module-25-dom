3cae4ce8316bdc23a424d092ca6b3cc9ea1577bf
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-10-14 01:32:02 +0600

    first commit

---
+++ events.js
@@ -0,0 +1,12 @@
+const makePurplebtn = document.getElementById('make-purple');
+makePurplebtn.onclick = function makePurple(){
+    document.body.style.backgroundColor = 'purple'
+}
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}  
+ const makeBluebtn = document.getElementById('make-blue')
+makeBluebtn.onclick = makeBlue;
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
